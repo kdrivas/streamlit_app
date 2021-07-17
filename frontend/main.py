2adc1c85a767dd9f56359bf159430765800a6fa7
kervyRivas <a20112128@pucp.pe>
2021-07-16 23:38:21 -0500

    exposing

--- frontend/main.py
+++ frontend/main.py
@@ -20,8 +20,3 @@ st.write("Test App")
 file = st.file_uploader("Upload an image", type=['png', 'jpg'])
 style = st.selectbox("Select style", [i for i in STYLES.keys()])
 
-if st.button("submit") and file is not None and style is not None:
-	res = requests.post(f'http://inference:8080/styles/{STYLES[style]}', files={'file': file}, )
-	img = BytesIO(res.content)
-	new_image = Image.open(img)
-	st.image(new_image, width=500)
