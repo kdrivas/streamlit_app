8645f15fd3e44d0611057710384e0633b9c7252d
kervyRivas <a20112128@pucp.pe>
2021-07-17 00:00:17 -0500

    exposing

--- frontend/run_streamlit.sh
+++ frontend/run_streamlit.sh
@@ -1,12 +1,2 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-enableXsrfProtection = false\n\
-\n\
-" > ~/.streamlit/config.toml
-
+#!/bin/bash
 streamlit run main.py
