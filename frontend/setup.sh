a03fae9e750db65290c3af90150e69fd2d8191d1
kervyRivas <a20112128@pucp.pe>
2021-07-18 01:06:22 -0500

    exposing

--- frontend/setup.sh
+++ frontend/setup.sh
@@ -3,6 +3,8 @@ mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+enableXsrfProtection = false\n\
+\n\
 " > ~/.streamlit/config.toml
