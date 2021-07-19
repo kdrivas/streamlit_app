3eaccd1bf8f9dace023082eae5405feb0e4a7e62
kervyRivas <a20112128@pucp.pe>
2021-07-18 20:05:39 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ import os
 from setuptools import setup
 
 thelibFolder = os.path.dirname(os.path.realpath(__file__))
-paths = [thelibFolder + 'inference/requirements.txt', thelibFolder + 'frontend/requirements.txt']
+paths = ['inference/requirements.txt', 'frontend/requirements.txt']
+install_requires = []
 for requirementPath in paths:
 	if os.path.isfile(requirementPath):
 			with open(requirementPath) as f:
-					install_requires = f.read().splitlines()
+					install_requires.extend(f.read().splitlines())
 
 setup(name="yourpackage", install_requires=install_requires)
